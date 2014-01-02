8666ac18854cec6ac1473c79d9f2f68fa9d5f40a
Chance Bliss <chancebliss@gmail.com>
2014-01-02 15:04:31 -0500

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #required for Heroku
+  config.action_mailer.default_url_options = { :host => 'http://chance-bliss.herokuapp.com/' }
 end
